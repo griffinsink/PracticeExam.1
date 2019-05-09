d37695ce68a3e147e90f1507b022ec5d2badc24a
Griffin Sink <Griffin.D.Sink@ou.edu>
2019-05-09 11:33:14 -0500

    hey ya

---
+++ JSONPracticeFinal/JSONPracticeFinal/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace JSONPracticeFinal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var response = client.GetAsync($"https://swapi.co/api/people").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = response.Content.ReadAsStringAsync().Result;
+                    var star = JsonConvert.DeserializeObject<List<Starwars>>(content);
+
+                    foreach(var x in star)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ JSONPracticeFinal/JSONPracticeFinal/Starwars.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONPracticeFinal
+{
+    class Starwars
+    {
+        public string name {get; set;}
+        public double height{get; set;}
+        public double mass {get; set;}
+        public string hair_color {get; set;}
+        public string skin_color {get; set;}
+        public string eye_color {get; set;}
+        public string birth_year {get; set;}
+        public string gender {get; set;}
+    }
+}
